f2c70224697c6532035eccc69fdb5bde8c8b9379
idanoo <daniel@m2.nz>
2023-01-30 12:33:16 +1300

    Order by counts

--- internal/gomastodonstats/main.go
+++ internal/gomastodonstats/main.go
@@ -1,6 +1,9 @@
 package gomastodonstats
 
-import "log"
+import (
+	"log"
+	"sort"
+)
 
 func Run() {
 	log.Println("Fetching counts")
@@ -14,7 +17,12 @@ func Run() {
 	// Write to DB
 	updatedMetrics := persistMetrics(metrics)
 	if len(updatedMetrics) > 0 {
-		// sendToMatrix(updatedMetrics)
+		// Sort by counts!
+		sort.Slice(updatedMetrics, func(i, j int) bool {
+			return updatedMetrics[i].MetricValue < updatedMetrics[j].MetricValue
+		})
+
+		sendToMatrix(updatedMetrics)
 		postToMastodon(updatedMetrics)
 	}
 }
